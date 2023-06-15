30b4d46ed2b6ede47a6e15781628eb02f4fd47db
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-06-15 19:51:18 +0300

    init

--- js/index.js
+++ js/index.js
@@ -56,32 +56,33 @@ function countryList(contrys) {
   });
 }
 
-function countryInfo(contrys) {
-  refs.list.innerHTML = "";
-  refs.search.blur();
-  refs.info.innerHTML = `
-  <h2>${contrys.name.official}</h2>
-  <div class="info__box">
-  <img src="${contrys.flags.svg}" height="100"/>
-  <ul class="info__list">
-  <li class="info__item">Capital: ${contrys.capital}</li>
-  <li class="info__item">Area: ${(contrys.area / 1000).toFixed(
-    2
-  )} thousand km²</li>
-  <li class="info__item">Population: ${(contrys.population / 1000000).toFixed(
-    2
-  )} mln people</li>
-  <li class="info__item">Languages: ${Object.values(contrys.languages)}</li>
-</ul>
-</div>`;
-}
+// function countryInfo(contrys) {
+//   refs.list.innerHTML = "";
+//   refs.search.blur();
+//   refs.info.innerHTML = `
+//   <h2>${contrys.name.official}</h2>
+//   <div class="info__box">
+//   <img src="${contrys.flags.svg}" height="100"/>
+//   <ul class="info__list">
+//   <li class="info__item">Capital: ${contrys.capital}</li>
+//   <li class="info__item">Area: ${(contrys.area / 1000).toFixed(
+//     2
+//   )} thousand km²</li>
+//   <li class="info__item">Population: ${(contrys.population / 1000000).toFixed(
+//     2
+//   )} mln people</li>
+//   <li class="info__item">Languages: ${Object.values(contrys.languages)}</li>
+// </ul>
+// </div>`;
+// }
 
 function restoreHtml() {
   refs.search.value = "";
-  refs.info.innerHTML = "";
+  // refs.info.innerHTML = "";
   refs.list.innerHTML = "";
 }
 
 refs.search.focus();
 refs.search.addEventListener("input", debounce(searchContry, DEBOUNCE_DELAY));
 refs.search.addEventListener("focus", restoreHtml);
+// wordList
